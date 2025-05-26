dd53f6ad4a659dc7d22ce9670a4984449bcbd8b0
Orestes Carracedo <orestes@orestes.io>
2025-05-26 16:29:29 +0200

    build(tslint): enable type-aware lint rules
    recommended by vite

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,11 +6,19 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+        ...tseslint.configs.recommendedTypeChecked,
+        ...tseslint.configs.strictTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+        parserOptions: {
+            project: ['./tsconfig.node.json', './tsconfig.app.json'],
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
     plugins: {
       'react-hooks': reactHooks,
