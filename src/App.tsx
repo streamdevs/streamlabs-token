84ce7dc4635fe5b08503d463fd1ece682c8ba02e
Orestes Carracedo <orestes@orestes.io>
2025-05-29 23:39:13 +0200

    feat: implement Streamlabs API POST /api/token call
    bring back original implementation by @santiMA10
    avoid dependency on qs in favor of standard
    flag redirected requests as coming from Netlify
    provide feedback for incorrect configurations
    use REDIRECT_URI from environment variable
    explain the security risk of exposing the OAuth client_secret
    use v2.0 of the Streamlabs API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Alternatively, we should have streamdevs-streamlabs-cli to just ask for the client_id and secret, and return the token
+  // This is a self-hosted web app. Only the developer is going to see this page
+  const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
+
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+  const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+
+  const [token, setToken] = useState<string | null>(null);
+  const [showToken, setShowToken] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  if (!CLIENT_ID || !CLIENT_SECRET || !REDIRECT_URI) {
+    return (
+      <main>
+        <header>
+          Configuration error
+          <p><code>VITE_CLIENT_ID</code>, <code>VITE_CLIENT_SECRET</code> and <code>VITE_REDIRECT_URI</code> must be provided as environment variables.
+          </p>
+        </header>
+      </main>
+    )
+  }
+
+  const apiUrl = "https://streamlabs.com/api/v2.0/";
+  const authorizeUrl = `${apiUrl}authorize?response_type=code&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&scope=alerts.create`;
+  const code = new URLSearchParams(window.location.search).get("code");
+
+  if (code && code.length > 0 && !token) {
+    const formData = new FormData();
+    formData.append("code", code);
+    formData.append("grant_type", "authorization_code");
+    formData.append("client_id", CLIENT_ID);
+    formData.append("client_secret", CLIENT_SECRET);
+    formData.append("redirect_uri", REDIRECT_URI);
+
+    const fetchToken = async () => {
+      const response = await fetch(`/api/token`, {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json() as { access_token: string | undefined };
+      const {access_token} = data;
+
+      setToken(access_token ?? null);
+    };
+
+    fetchToken().catch((e: unknown) => {
+      setErrorMessage((e as Error).message)
+    });
+  }
+
+  if (errorMessage) {
+    return (
+      <main>
+        <header>
+          An error occurred fetching the Streamlabs token:
+          <p>{errorMessage}</p>
+        </header>
+      </main>
+    )
+  }
+
+  if (token) {
+    return (
+      <main>
+        <header>
+          <input type={showToken ? "text" : "password"} defaultValue={token}/>
+          <button type="button" onClick={() => {
+            setShowToken(!showToken)
+          }}>
+            {showToken ? "Hide token" : "Show token"}
+          </button>
+        </header>
+      </main>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <main className="App">
+      <header>
+        <a style={{color: "white"}} href={authorizeUrl}>
+          Login with Streamlabs
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+    </main>
+  );
 }
 
 export default App
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+// @see https://vite.dev/guide/env-and-mode.html#intellisense-for-typescript
+interface ImportMetaEnv {
+    VITE_CLIENT_ID: string | undefined;
+    VITE_CLIENT_SECRET: string | undefined;
+    VITE_REDIRECT_URI: string | undefined;
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
